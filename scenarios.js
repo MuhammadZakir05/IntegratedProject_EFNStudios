67caac2e8fa87bafbea118af241c8bdfdd22ee25
joshua <joshloke2004@gmail.com>
2026-01-27 10:27:34 +0800

    start of id segment

---
+++ scenarios.js
@@ -0,0 +1,12 @@
+const scenarios = [
+    {
+        id: 1,
+        sender: "Unknown",
+        message: "Hello! I am Recruiter from EFN. You are hired! Click here to claim $500: http://fake-link.com",
+        choices: [
+            { text: "Click Link", isCorrect: false },
+            { text: "Block & Report", isCorrect: true },
+            { text: "Reply: 'Who is this?'", isCorrect: false }
+        ]
+    }
+];
